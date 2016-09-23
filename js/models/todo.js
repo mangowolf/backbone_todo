85540f4a574f1420e5a2ffe549dbd8ceeb746acc
mangowolf <jimmyz949@hotmail.com>
2016-09-22 21:59:51 -0700

    Templates Script, model, and collections written

---
+++ js/models/todo.js
@@ -0,0 +1,19 @@
+/*---------Todo Model----------*/
+
+var app = app || {};
+
+app.Todo = Backbone.Model.extend({
+	//Default attributes ensure that each todo created has a 'title' and
+	//'completed' keys.
+	defaults:{
+		title: '',
+		completed: false
+	},
+
+	//Toggle the 'completed' state of this todo item.
+	toggle: function(){
+		this.save({
+			completed: !this.get('completed')
+		});
+	}
+});
